7746f21e55a23bca91f8f055fe3d0ac3a26ca395
Thongchai Subsaidee <chanoktrue@me.com>
2021-04-22 09:27:48 +0700

    first project

---
+++ ListViewSection/ListViewSection/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ListViewSection
+//
+//  Created by Thongchai Subsaidee on 22/4/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ ListViewSection/ListViewSection/ListViewSectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewSectionApp.swift
+//  ListViewSection
+//
+//  Created by Thongchai Subsaidee on 22/4/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct ListViewSectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
